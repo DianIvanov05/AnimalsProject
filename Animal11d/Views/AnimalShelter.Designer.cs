a28f53d15ade705f8ea2fec33db36a6094fb28e7
Krisi2312 <114053531+Krisi2312@users.noreply.github.com>
2023-06-09 12:07:31 +0300

    first commit

---
+++ Animal11d/Views/AnimalShelter.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Animal11d.Controllers
+{
+    partial class AnimalShelter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb_Id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb_Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txb_Desc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cmb_Breed = new System.Windows.Forms.ComboBox();
+            this.txb_Age = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(238, 23);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(346, 31);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Приют за изоставени котки";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // txb_Id
+            // 
+            this.txb_Id.Location = new System.Drawing.Point(64, 93);
+            this.txb_Id.Name = "txb_Id";
+            this.txb_Id.Size = new System.Drawing.Size(100, 22);
+            this.txb_Id.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Име";
+            // 
+            // txb_Name
+            // 
+            this.txb_Name.Location = new System.Drawing.Point(85, 148);
+            this.txb_Name.Name = "txb_Name";
+            this.txb_Name.Size = new System.Drawing.Size(100, 22);
+            this.txb_Name.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 27);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Описание";
+            // 
+            // txb_Desc
+            // 
+            this.txb_Desc.Location = new System.Drawing.Point(132, 192);
+            this.txb_Desc.Name = "txb_Desc";
+            this.txb_Desc.Size = new System.Drawing.Size(100, 22);
+            this.txb_Desc.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 27);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Възраст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 27);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Порода";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добави";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Обнови";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Изтрий Избраното";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(146, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 57);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Открий по Id";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // cmb_Breed
+            // 
+            this.cmb_Breed.FormattingEnabled = true;
+            this.cmb_Breed.Location = new System.Drawing.Point(132, 280);
+            this.cmb_Breed.Name = "cmb_Breed";
+            this.cmb_Breed.Size = new System.Drawing.Size(100, 24);
+            this.cmb_Breed.TabIndex = 3;
+            this.cmb_Breed.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txb_Age
+            // 
+            this.txb_Age.Location = new System.Drawing.Point(132, 235);
+            this.txb_Age.Name = "txb_Age";
+            this.txb_Age.Size = new System.Drawing.Size(100, 22);
+            this.txb_Age.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(417, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(388, 228);
+            this.listBox1.TabIndex = 4;
+            // 
+            // AnimalShelter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 508);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cmb_Breed);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txb_Age);
+            this.Controls.Add(this.txb_Desc);
+            this.Controls.Add(this.txb_Name);
+            this.Controls.Add(this.txb_Id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "AnimalShelter";
+            this.Text = "AnimalShelter";
+            this.Load += new System.EventHandler(this.AnimalShelter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb_Id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb_Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txb_Desc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox cmb_Breed;
+        private System.Windows.Forms.TextBox txb_Age;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
